96b9f96cfeec08df0d5d64de175b892395a40cf4
AuroraStarry <QinJun@email.cn>
2023-03-11 15:36:42 +0800

    添加 IO 工具类

---
+++ starry-core/src/main/java/cn/starrys/core/util/FileUtils.java
@@ -0,0 +1,10 @@
+package cn.starrys.core.util;
+
+/**
+ * creationTime: 2023/3/10 16:03 .
+ *
+ * @author XingKong
+ */
+public interface FileUtils {
+
+}
---
+++ starry-core/src/main/java/cn/starrys/core/util/IOUtils.java
@@ -0,0 +1,13 @@
+package cn.starrys.core.util;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * creationTime: 2023/3/11 10:19 .
+ *
+ * @author XingKong
+ */
+@Slf4j
+public class IOUtils {
+
+}
