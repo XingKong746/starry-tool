5f35a9716272729ade3768df6570bde90be88ca9
XingKong <xkhi@qq.com>
2024-12-20 20:26:38 +0800

    优化仓库镜像配置

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,9 +21,7 @@ allprojects {
         mavenLocal()
         maven { name = "tencentyun-maven"; url = uri("https://mirrors.tencent.com/maven") }
         maven { name = "aliyun-google-maven"; url = uri("https://maven.aliyun.com/repository/google") }
-        maven { name = "aliyun-gradle-plugin"; url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
         mavenCentral()
-        gradlePluginPortal()
         google()
     }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,7 @@
 buildscript {
     repositories {
-        mavenLocal()
         maven { name = "aliyun-gradle-plugin"; url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
-        maven { name = "tencentyun-maven"; url = uri("https://mirrors.tencent.com/maven") }
-        maven { name = "aliyun-google-maven"; url = uri("https://maven.aliyun.com/repository/google") }
         gradlePluginPortal()
-        mavenCentral()
-        google()
     }
 }
 
